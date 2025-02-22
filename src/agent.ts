4013d19a267d446474d36c783f93391577960b4e
Drifta01 <carlguilford@gmail.com>
2025-02-22 16:57:32 +1300

    initial

--- src/agent.ts
+++ src/agent.ts
@@ -22,31 +22,4 @@ export const runAgent = async ({
   ])
 
   const loader = showLoader('Thinking...')
-
-  while (true) {
-    const history = await getMessages()
-    const response = await runLLM({
-      messages: history,
-      tools,
-    })
-
-    await addMessages([response])
-
-    logMessage(response)
-
-    if (response.content) {
-      loader.stop()
-      return getMessages()
-    }
-
-    if (response.tool_calls) {
-      const toolCall = response.tool_calls[0]
-      loader.update(`executing: ${toolCall.function.name}`)
-
-      const toolResponse = await runTool(toolCall, userMessage)
-      await saveToolResponse(toolCall.id, toolResponse)
-
-      loader.update(`executed: ${toolCall.function.name}`)
-    }
-  }
 }
