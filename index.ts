6febb9ed0a738494acd0134225b381a11b1b7e84
Drifta01 <carlguilford@gmail.com>
2025-02-23 09:00:29 +1300

    step/2

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
-import { runLLM } from './src/llm'
+import { runAgent } from './src/agent'
+import { tools } from './src/tools'
+
 const userMessage = process.argv[2]
 
 if (!userMessage) {
@@ -7,6 +9,7 @@ if (!userMessage) {
   process.exit(1)
 }
 
-const response = await runLLM({ userMessage })
-
-console.log(response)
+const messages = await runAgent({
+  userMessage,
+  tools,
+})
